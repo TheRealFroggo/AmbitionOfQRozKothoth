94841278a526df9c3d4d295b9899e61a6242027c
Benjamin Keefe <bakabenja@gmail.com>
2021-05-12 04:35:27 +0900

    First prototype commit
    Add player that move left to right and follow camera

---
+++ Assets/Scenes/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float m_Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float XMovement = Input.GetAxis("Horizontal") * m_Speed * Time.deltaTime;
+        transform.position += new Vector3(XMovement, 0,0);
+    }
+}
